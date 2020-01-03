8bce5fa977913dc1bdeaf2f3ee5e1879f669e624
BLECUONA <benjaminlecuona@gmail.com>
2020-01-03 20:13:14 +0100

    [DEV](dropZone) added a fake drop component

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { makeStyles, Grid } from '@material-ui/core';
 import { Items } from '../utils/localStorageItems';
+import DropZoneArea from '../components/DropZoneArea';
 
 const Dashboard: React.FC = () => {
     const classes = useStyles();
@@ -14,6 +15,7 @@ const Dashboard: React.FC = () => {
     return (
         <Grid container className={classes.body}>
             <h1>{`Welcome to your dashboard ${nickname?.toUpperCase()}`}</h1>
+            <DropZoneArea ButtonTextHover="Add files"/>
         </Grid>
     );
 }
